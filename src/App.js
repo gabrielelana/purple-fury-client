15a886cd7622e2c8dc32c573b22580e7006c97b7
gabriele.lana <gabriele.lana@cleancode.it>
2018-06-12 14:50:40 +0200

    Login, connect socket and post a message ;-)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import openSocket from 'socket.io-client'
+
+fetch('https://purple-fury.now.sh/login', {
+  method: 'post',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    username: "gabriele",
+    password: "secret",
+  })
+}).then(res => res.json())
+  .then(
+    res => {
+      console.log(res)
+      const socket = openSocket(`https://purple-fury.now.sh/?token=${res.token}`)
+      socket.on('messages', data => {
+        console.log('from-socket', data)
+      })
+
+      fetch('https://purple-fury.now.sh/messages', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          token: res.token,
+          message: "Hello World!",
+        })
+      })
+    }
+  )
+
 class App extends Component {
   render() {
     return (
